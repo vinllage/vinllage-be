7c84f657b23c9d2596cf413812cf4a1d9baf63f7
songmikim <50817231+songmikim@users.noreply.github.com>
2025-08-19 00:36:21 +0900

    환경행사 서비스,컨트롤러 추가

--- src/main/java/xyz/vinllage/crawlSource/repositories/CrawlSourceRepository.java
+++
@@ -1,8 +0,0 @@
-package xyz.vinllage.crawlSource.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import xyz.vinllage.crawlSource.entities.CrawlSource;
-
-public interface CrawlSourceRepository extends JpaRepository<CrawlSource, Long> {
-
-}
--- src/main/java/xyz/vinllage/event/repositories/EventRepository.java
+++
@@ -1,7 +0,0 @@
-package xyz.vinllage.event.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import xyz.vinllage.event.entities.Event;
-
-public interface EventRepository extends JpaRepository<Event, Long> {
-}
