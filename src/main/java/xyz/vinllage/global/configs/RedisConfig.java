50b06c0823b45d3060242edc0ffc06542a754281
Daeun-Oh <90241178+Daeun-Oh@users.noreply.github.com>
2025-08-27 23:41:52 +0900

    필요 없는 코드 삭제

--- src/main/java/xyz/vinllage/global/configs/RedisConfig.java
+++ src/main/java/xyz/vinllage/global/configs/RedisConfig.java
@@ -4,9 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @Configuration
@@ -18,28 +16,8 @@ public class RedisConfig {
     @Value("${spring.data.redis.port}")
     private int port;
 
-    /**
-     *  RedisTemplate Bean
-     *  Redis 에 데이터를 저장 할때 직렬화 / 역직력화 밥법으로 설정
-     * key 는 문자열 (String) , Value는 json 으로 저장
-     *
-     */
     @Bean
-    public RedisTemplate<String , Object> redisTemplate(RedisConnectionFactory redisConnectionFactory){
-        RedisTemplate<String , Object> template = new RedisTemplate<>();
-        template.setConnectionFactory(redisConnectionFactory);
-        // 직력화 역 직렬화 조회 삭제 수정 데이터를 불러 오는 것
-        template.setKeySerializer(new StringRedisSerializer());
-        template.setHashKeySerializer(new StringRedisSerializer());
-
-        // value 직력화 GenericJackson2JsonRedisSerializer Spring Data Redis 제공 하는 jso 직력화 역 직렬화 도구
-        GenericJackson2JsonRedisSerializer serializer = new GenericJackson2JsonRedisSerializer();
-        template.setValueSerializer(serializer);
-        template.setHashValueSerializer(serializer);
-
-        template.afterPropertiesSet();
-        return template;
-
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory(host, port);
     }
-
 }
